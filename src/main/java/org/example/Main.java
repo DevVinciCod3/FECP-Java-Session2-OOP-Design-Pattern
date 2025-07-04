a13d770353eaa8e112e0ddf6b94dd71d50fb6a71
Harold Bryan Santos <harold.santos@GXCHLAP00690.local>
2025-07-04 12:44:30 +0800

    Initial Commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,65 @@
 package org.example;
-
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.*;
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+    private static boolean isRunning = true;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        while (isRunning) {
+            System.out.println("\n=== G-Ride Booking System ===");
+            System.out.println("1. Book a ride");
+            System.out.println("2. Calculate Fare");
+            System.out.println("3. View Receipt ");
+            System.out.println("4. Exit");
+            System.out.print("Enter choice (1-4): ");
+
+            int option = sc.nextInt();
+            sc.nextLine();
+
+            switch (option) {
+                case 1 -> bookARide(sc);
+                case 2 -> calculateFare(sc);
+                case 3 -> viewReceipt();
+                case 4 -> {
+                    System.out.println("=== Thank You!! ===");
+                    isRunning = false;
+                    return;
+                }
+                default -> System.out.println("Invalid choice.");
+            }
         }
+    }
+
+    private static void bookARide(Scanner sc) {
+        System.out.print("Enter Ride Type (Standard/Premium): ");
+        String rideType = sc.nextLine();
+        System.out.print("Enter Distance (km): ");
+        int distance = sc.nextInt();
+        sc.nextLine();
+        System.out.print("Enter Duration (mins): ");
+        int duration = sc.nextInt();
+        sc.nextLine();
+        System.out.println("Ride Booked Successfully!");
+    }
+
+    private static void calculateFare(Scanner sc) {
+        System.out.print("Fare Type (Normal / Night): ");
+        String fareType = sc.nextLine();
+
+        System.out.println("Base Fare: ");
+        System.out.println("Distance Cost: ");
+        System.out.println("Duration Cost: ");
+        System.out.println("Surcharge (Night): ");
+        System.out.println("Total Fare: ");
+
+    }
+
+    private static void viewReceipt() {
+        System.out.println("=== Receipt ===");
+        System.out.println("Ride Type: ");
+        System.out.println("Distance: ");
+        System.out.println("Duration: ");
+        System.out.println("Total Fare: ");
     }
 }
